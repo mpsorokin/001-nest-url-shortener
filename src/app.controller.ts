15e6b4961f7ba0e75b8c91223c26f22341467ef4
Aa <aa@example.com>
2025-05-05 15:51:22 +0300

    update structure

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import { ClientIp, UserAgent } from './common/decorators/http.decorator';
 
 @Controller()
 export class AppController {
@@ -10,10 +11,12 @@ export class AppController {
   async getLinkByShortCode(
     @Param('code') code: string,
     @Res({ passthrough: true }) res: Response,
+    @ClientIp() ip: string,
+    @UserAgent() userAgent: string,
   ) {
     const link = await this.appService.getLinkByShortCode(code);
 
-    await this.appService.trackClick(link.shortCode, '1', 'User Agent');
+    await this.appService.trackClick(link.shortCode, ip, userAgent);
 
     return res.redirect(link.originalUrl);
   }
