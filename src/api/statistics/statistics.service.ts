cda79b59a56bb1243db4737bafcb1fd6c5fb2809
Aa <aa@example.com>
2025-05-05 16:08:33 +0300

    update structure

--- src/api/statistics/statistics.service.ts
+++ src/api/statistics/statistics.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../infra/prisma/prisma.service';
+import { UAParser } from 'ua-parser-js';
+import { lookup } from 'geoip-country';
 
 @Injectable()
 export class StatisticsService {
-  constructor(private readonly prismaService: PrismaService) {}
+  private readonly parser: UAParser;
+
+  constructor(private readonly prismaService: PrismaService) {
+    this.parser = new UAParser();
+  }
 
   private async getClicks(linkId: string) {
     const clicks = await this.prismaService.click.findMany({
@@ -13,5 +19,18 @@ export class StatisticsService {
     });
 
     return clicks;
+  }
+
+  private getBrowserByUserAgent(userAgent: string) {
+    this.parser.setUA(userAgent);
+
+    const result = this.parser.getResult();
+
+    return { browser: result.browser };
+  }
+
+  private getCountryByIp(ip: string) {
+    const geo = lookup(ip);
+    console.log(geo);
   }
 }
