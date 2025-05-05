2080d081fc9e2eb95d56d3f783df25f8cff2f7e0
Aa <aa@example.com>
2025-05-05 13:47:22 +0300

    update structure

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { LinkModule } from './link/link.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, LinkModule],
 })
 export class ApiModule {}
